48ead9508b984789caa6bc3d8033e2b3811d965e
Manuel Bauer <maba0809@gmail.com>
2020-04-05 11:31:41 +0200

    on click direction

--- Assets/Scripts/GridNode.cs
+++ Assets/Scripts/GridNode.cs
@@ -8,12 +8,16 @@ public class GridNode
   public Color Color { get; set; }
   public bool Occupied { get; set; }
   public List<GridNode> NearNodes { get; set; }
+  public float DistanceToTarget { get; set; } = 0f;
+  public float DistanceToSource { get; set; } = 0f;
+  public Vector3 CenterPoint { get; }
 
   public GridNode(Vector3 position, Vector3 size)
   {
     Position = position;
     Size = size;
     NearNodes = new List<GridNode>();
+    CenterPoint = new Vector3(Position.x + Size.x / 2, Position.y, Position.z + Size.z / 2);
 
     ResetColor();
   }
--- Assets/Scripts/Pathfinding.cs
+++ Assets/Scripts/Pathfinding.cs
@@ -1,19 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 public class Pathfinding : MonoBehaviour
 {
-  public GridNode CurrentNode;
   public Grid NodeGrid;
+  private GridNode mCurrentNode;
 
   // Start is called before the first frame update
   void Start()
   {
     if (NodeGrid != null)
     {
-      var currentNode = NodeGrid.GetNodeFromPosition(transform.position.x, transform.position.z);
+      mCurrentNode = NodeGrid.GetNodeFromPosition(transform.position.x, transform.position.z);
 
-      NodeGrid.UpdateNode(currentNode, node => node.Color = Color.green);
-      foreach(var nearNode in currentNode.NearNodes)
+      NodeGrid.UpdateNode(mCurrentNode, node => node.Color = Color.green);
+      foreach(var nearNode in mCurrentNode.NearNodes)
       {
         NodeGrid.UpdateNode(nearNode, node => node.Color = Color.yellow);
       }
@@ -23,5 +24,11 @@ public class Pathfinding : MonoBehaviour
   public void OnGridClicked(GridNode node)
   {
     NodeGrid.UpdateNode(node, updateNode => updateNode.Color = Color.blue);
+    foreach(var nearNode in mCurrentNode.NearNodes)
+    {
+      nearNode.DistanceToSource = 0;
+      nearNode.DistanceToTarget = Vector3.Distance(nearNode.CenterPoint, node.CenterPoint);
+    }
+    NodeGrid.UpdateNode(mCurrentNode.NearNodes.First(x => x.DistanceToTarget == mCurrentNode.NearNodes.Min(y => y.DistanceToTarget)), updateNode => updateNode.Color = Color.black);
   }
 }
